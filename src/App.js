0089a2f563c9dc05553783ce0acf5ea5ec1aa54f
Ruchit Karnawat <ruchitkarnawat1999@gmail.com>
2021-02-28 16:16:31 +0530

    no external dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
 import './App.css';
 
 
@@ -9,34 +6,85 @@ import './App.css';
 export class App extends Component {
   state = {
     data : [],
+    id: 1,
+    name: '',
+    knownFor: '',
+    image: '',
+    url: ''
   }
 
   componentDidMount() {
-    axios.get('/influencer').then( res => {
-      console.log(res.data);
-      this.setState({data : res.data});
-      console.log(this.state.data);
-    })
+    fetch('/influencer').then(response => 
+      response.json()).then(resData => {
+        this.setState({data : resData,
+           knownFor : resData[0].knownFor,
+           image : resData[0].image,
+           url : resData[0].url,
+           name : resData[0].name
+          });
+      console.log(this.state.name);
+      })
 }
 
+onprev = ()=>{
+  let id = this.state.id - 1;
+  let count = this.state.data.length;
+  id = id + count - 1;
+  id = id % count;
+  id++;
+  console.log(this.state.data[id - 1]);
+  this.setState({id : id,
+    knownFor : this.state.data[id - 1].knownFor,
+    image : this.state.data[id - 1].image,
+    url : this.state.data[id - 1].url,
+    name : this.state.data[id - 1].name
+   });
+}
+
+onprev = ()=>{
+  let id = this.state.id - 1;
+  let count = this.state.data.length;
+  id = id + count - 1;
+  id = id % count;
+  id++;
+  console.log(this.state.data[id - 1]);
+  this.setState({id : id,
+    knownFor : this.state.data[id - 1].knownFor,
+    image : this.state.data[id - 1].image,
+    url : this.state.data[id - 1].url,
+    name : this.state.data[id - 1].name
+   });
+}
+
+onnext = ()=>{
+  let id = this.state.id - 1;
+  let count = this.state.data.length;
+  id = (id + 1) % count;
+  id++;
+  console.log(this.state.data[id - 1]);
+  this.setState({id : id,
+    knownFor : this.state.data[id - 1].knownFor,
+    image : this.state.data[id - 1].image,
+    url : this.state.data[id - 1].url,
+    name : this.state.data[id - 1].name
+   });
+}
 
   render() {
     return (
-      <>
-      <span style={{color:'white',zIndex: '2', position:"fixed",top:"2%", left:"2%"}}>Storybook</span>
+      <div data-testid={'story-' + this.state.id} className="mySlides" style={{display: 'flex', maxWidth: '1000px', margin: 'auto'}}>
+        <div className="app-title">Storybook</div>
 
-      <Carousel dynamicHeight={false} showArrows={true} showStatus={false} showIndicators={false} infiniteLoop={true} showThumbs={false} >
-                {this.state.data.map((item) => (
-                   <>
-                   <img src={item.image} style={{padding: '5px', maxWidth:"500px", height:"auto", width:"100%" }} alt=""></img>
-                   <a href={item.url}><h2 style={{ maxWidth:"20em",margin: 'auto',position:"absolute", zIndex: '2',color:'white',top:"50%",left:"30%", background:'grey'}}>{item.name}</h2></a>
-                  <h3 style={{ maxWidth:"25em",position:"absolute", zIndex: '2',color:'white',top:"60%",left:"30%",background:'grey' }}>{item.knownFor}</h3>
-                  </>
-                ))
-                }
-      </Carousel>
-      </>
-    )
+        <div className="prev" data-testid = "prev" style={{left:'0'}} onClick = {this.onprev}>&lt;</div>
+      <div className="next" data-testid="next" onClick = {this.onnext}>&gt;</div>
+
+          <img src={this.state.image} style={{maxWidth:"600px", height:"auto", width:"100%", paddingLeft: '30px', paddingRight: '30px'}}></img>
+          <div className="heading-text">
+          <a href={this.state.url} style={{textDecoration:'none', color:'white'}}><div className="title" data-testid={"title-" + this.state.id}>{this.state.name ? this.state.name : 'Mohandas Karamchand Gandhi'}</div></a>
+          <div className="known-for">{this.state.knownFor ? this.state.knownFor : "Leadership of the campaign for India's independence from British rule, Nonviolent resistance"}</div>
+          </div>
+      </div>
+      )
   }
 }
 
